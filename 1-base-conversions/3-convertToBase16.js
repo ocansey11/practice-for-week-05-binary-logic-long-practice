64f440db18200e5cd010a604ede5f4c51b3025b8
kevin ocansey <kevinocansey11@gmail.com>
2022-12-28 11:41:05 +0000

    completed convertToBase16 function

--- 1-base-conversions/3-convertToBase16.js
+++ 1-base-conversions/3-convertToBase16.js
@@ -4,6 +4,38 @@
 
 const convertToBase16 = element => {
   // Your code here
+  let total = 0
+  let hex = '0x'
+  const base2 = 2
+
+  if(typeof element == 'number' ){
+    hex += element.toString(16)
+
+    return hex
+  }
+
+  if(typeof element == 'string'){
+    // first convert to Base10
+    let newArr = element.split('')
+    if(newArr[0]== 0 && newArr[1] == 'b'){
+      // check no other string appears within the values
+      for(let i = 2; i < newArr.length; i++){
+        if(!(newArr[i] === '1' || newArr [i] === '0')){
+          throw TypeError("invalid binary value  : "  + newArr[i])
+        }
+      }
+
+      for(let i = newArr.length - 1; i > 1; i--){
+        let power = (newArr.length-1) - i
+        let value  = parseInt(newArr[i])
+
+        total += (base2 ** power) * value
+      }
+      // convert to base16
+      hex += total.toString(16)
+      return hex
+    }
+  }
 };
 
 /******************************************************************************/
